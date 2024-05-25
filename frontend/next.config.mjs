01988a017a016c49c04701f99d078991e8de9f0b
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-05-25 14:18:33 +0300

    initial commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: '/api/:path*',
+        },
+      ];
+    },
+  };
